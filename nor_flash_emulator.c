7293143c89e51ddf6e579117a0eba9fa90347c88
Lespagnol <lespagnolj@gmail.com>
2021-11-11 15:40:30 +0100

    config tests
    First files created to test VSCode extensions configuration : formatter ok, doc generator OK

---
+++ nor_flash_emulator.c
@@ -0,0 +1,24 @@
+/**
+ * @file nor_flash_emulator.c
+ * @author Julien Lespagnol (lespagnolj@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-11-11
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+#include <stdbool.h>
+
+/**
+ * @brief
+ *
+ * @return int
+ */
+int nor_flash_emulator_init(void)
+{
+    if (true)
+    {
+        return 0;
+    }
+}
---
+++ nor_flash_emulator.h
@@ -0,0 +1,15 @@
+/**
+ * @file nor_flash_emulator.h
+ * @author Julien Lespagnol (lespagnolj@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __NOR_FLASH_EMULATOR_H_
+#define __NOR_FLASH_EMULATOR_H_
+
+#endif
